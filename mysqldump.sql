78de1132fceebee679ebf70703fff1c357675d99
ArtyomZinchenko <artyom.zinchenko@gmail.com>
2019-08-05 09:25:58 +0200

    mysqldump
    Создайте дамп базы данных example из предыдущего задания, разверните содержимое дампа в новую базу данных sample.

---
+++ mysqldump.sql
@@ -0,0 +1,6 @@
+/*   Создайте дамп базы данных example из предыдущего задания, разверните содержимое дампа в новую базу данных sample. *
+ *//
+ 
+ mysqldump example > example_dump.sql
+ 
+ tail -30 example_dump.sql
