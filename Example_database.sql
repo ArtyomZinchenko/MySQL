a8437e79a12218e78662fa1416a5f8e5d0886489
ArtyomZinchenko <artyom.zinchenko@gmail.com>
2019-08-05 09:12:03 +0200

    Add files via upload
    Создайте базу данных example, разместите в ней таблицу users, состоящую из двух столбцов, числового id и строкового name.

---
+++ Example_database.sql
@@ -0,0 +1,18 @@
+/* Aryom Zinchdnko, HW1
+
+
+--Создайте базу данных example, разместите в ней таблицу users, состоящую из двух столбцов, числового id и строкового name.
+
+*/
+
+
+DROP DATABASE IF EXISTS example;
+CREATE DATABASE example;
+USE vk;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+	id BIGINT PRIMARY KEY, 
+    name VARCHAR(50),
+    
+);
